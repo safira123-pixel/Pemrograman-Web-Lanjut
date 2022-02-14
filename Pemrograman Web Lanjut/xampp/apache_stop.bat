ab378b0284577ee6bc43c333148068ec1a9d9a09
Safira Istifarini_TI <73090483+safira123-pixel@users.noreply.github.com>
2022-02-14 10:57:07 +0700

    Delete Pemrograman Web Lanjut/xampp directory

--- Pemrograman Web Lanjut/xampp/apache_stop.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /D %~dp0
-cmd.exe /C start "" /MIN call "D:\Fira\Kuliah\KULIAH TEKNIK INFORMATIKA\SEMESTER 3\Desain dan Pemrograman Web\xampp\killprocess.bat" "httpd.exe"
-if not exist apache\logs\httpd.pid GOTO exit
-del apache\logs\httpd.pid
-
-:exit
