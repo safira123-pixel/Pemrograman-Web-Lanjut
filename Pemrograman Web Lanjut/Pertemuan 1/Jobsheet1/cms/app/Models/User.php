404879f48c1f2520444538dc26e7ba3efb568083
Safira Istifarini_TI <73090483+safira123-pixel@users.noreply.github.com>
2022-03-21 20:31:39 +0700

    Praktikum 6

--- Pemrograman Web Lanjut/Pertemuan 1/Jobsheet1/cms/app/Models/User.php
+++ Pemrograman Web Lanjut/Pertemuan 1/Jobsheet1/cms/app/Models/User.php
@@ -18,7 +18,7 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'userame',
+        'username',
         'name',
         'email',
         'password',
