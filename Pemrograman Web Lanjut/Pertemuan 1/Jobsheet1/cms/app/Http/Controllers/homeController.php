9e1b167120a5fe3e67ee32ddc0222932d4b62106
Safira Istifarini_TI <73090483+safira123-pixel@users.noreply.github.com>
2022-03-21 19:42:05 +0700

    Praktikum 6

--- Pemrograman Web Lanjut/Pertemuan 1/Jobsheet1/cms/app/Http/Controllers/homeController.php
+++ Pemrograman Web Lanjut/Pertemuan 1/Jobsheet1/cms/app/Http/Controllers/homeController.php
@@ -6,7 +6,12 @@ use Illuminate\Http\Request;
 
 class homeController extends Controller
 {
-    public function index(){
-        return 'Hi, Selamat Datang!';
-    }
+    //  public function index(){
+    //     return 'Hi, Selamat Datang!';
+    //  }
+     public function index(){
+         return view('beranda');
+        // $user = Auth::user();
+         //return view('home', ['user' => $user]);
+     }
 }
