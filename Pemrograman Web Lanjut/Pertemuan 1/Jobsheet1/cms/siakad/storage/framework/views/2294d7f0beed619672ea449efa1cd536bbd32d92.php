b963679f0f4f364a1229f914a7c2c0a23cd94ae0
Safira Istifarini_TI <73090483+safira123-pixel@users.noreply.github.com>
2022-03-28 14:12:23 +0700

    praktikum jobsheet 7

---
+++ Pemrograman Web Lanjut/Pertemuan 1/Jobsheet1/cms/siakad/storage/framework/views/2294d7f0beed619672ea449efa1cd536bbd32d92.php
@@ -0,0 +1,59 @@
+
+
+<?php $__env->startSection('content'); ?>
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left mt-2">
+                <h2>JURUSAN TEKNOLOGI INFORMASI-POLITEKNIK NEGERI MALANG</h2>
+            </div>
+            <div class="float-right my-2">
+            <a class="btn btn-success" href="<?php echo e(route('mahasiswa.create')); ?>"> Input Mahasiswa</a>
+            </div>
+        </div>
+    </div>
+
+<?php if($message = Session::get('success')): ?>
+    <div class="alert alert-success">
+        <p><?php echo e($message); ?></p>
+    </div>
+<?php endif; ?>
+<?php if($message = Session::get('error')): ?>
+    <div class="alert alert-error">
+        <p><?php echo e($message); ?></p>
+    </div>
+<?php endif; ?>
+
+<table class="table table-bordered">
+    <tr>
+        <th>Nim</th>
+        <th>Nama</th>
+        <th>Kelas</th>
+        <th>Jurusan</th>
+        <th width="280px">Action</th>
+    </tr>
+<?php $__currentLoopData = $mahasiswa; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $mhs): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+    <tr>
+
+        <td><?php echo e($mhs ->nim); ?></td>
+        <td><?php echo e($mhs ->nama); ?></td>
+        <td><?php echo e($mhs ->kelas); ?></td>
+        <td><?php echo e($mhs ->jurusan); ?></td>
+        <td>
+            <form action="<?php echo e(route('mahasiswa.destroy',['mahasiswa'=>$mhs->nim])); ?>" method="POST">
+
+                <a class="btn btn-info" href="<?php echo e(route('mahasiswa.show',$mhs->nim)); ?>">Show</a>
+
+                <a class="btn btn-primary" href="<?php echo e(route('mahasiswa.edit',$mhs->nim)); ?>">Edit</a>
+
+                <?php echo csrf_field(); ?>
+                <?php echo method_field('DELETE'); ?>
+
+                <button type="submit" class="btn btn-danger">Delete</button>
+            </form>
+        </td>
+    </tr>
+    
+<?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+</table>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('mahasiswa.layout', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\Fira\Kuliah\KULIAH TEKNIK INFORMATIKA\SEMESTER 4\Pemrograman Web Lanjut\cms\siakad\resources\views/mahasiswa/index.blade.php ENDPATH**/ ?>
---
+++ Pemrograman Web Lanjut/Pertemuan 1/Jobsheet1/cms/siakad/storage/framework/views/47b0dfe516308099a3e11a02cee3fadfc2ff1e51.php
@@ -0,0 +1,24 @@
+
+
+<?php $__env->startSection('content'); ?>
+
+<div class="container mt-5">
+    <div class="row justify-content-center align-items-center">
+        <div class="card" style="width: 24rem;">
+            <div class="card-header">
+                Detail Mahasiswa
+            </div>
+        <div class="card-body">
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item"><b>Nim: </b><?php echo e($Mahasiswa->nim); ?></li>
+                <li class="list-group-item"><b>Nama: </b><?php echo e($Mahasiswa->nama); ?></li>
+                <li class="list-group-item"><b>Kelas: </b><?php echo e($Mahasiswa->kelas); ?></li>
+                <li class="list-group-item"><b>Jurusan: </b><?php echo e($Mahasiswa->jurusan); ?></li>
+            </ul>
+        </div>
+        <a class="btn btn-success mt-3" href="<?php echo e(route('mahasiswa.index')); ?>">Kembali</a>
+        </div>
+    </div>
+</div>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('mahasiswa.layout', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\Fira\Kuliah\KULIAH TEKNIK INFORMATIKA\SEMESTER 4\Pemrograman Web Lanjut\cms\siakad\resources\views/mahasiswa/detail.blade.php ENDPATH**/ ?>
